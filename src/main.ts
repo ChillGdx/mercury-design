3db61bb25057b3e0aee796bd78aa2b5eae24e775
Hasono <2105689721@qq.com>
2025-09-09 15:24:14 +0800

    移除html

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,3 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
 library.add(fas)
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
